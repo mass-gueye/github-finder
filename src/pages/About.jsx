d7bebc59245ca06b8a92b534a2139904778a245a
mass-gueye <mass-gueye@protonmail.com>
2022-04-03 23:57:55 +0000

    Editing the layout theme and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,10 @@ export default function About() {
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href="https://www.udemy.com/course/modern-react-front-to-back/">
-          {" "}
-          React Front To Back
-        </a>{" "}
-        Udemy course by
+        project
+        <a href="https://github.com/mass-gueye/github-finder"> repository</a> by
         <strong>
-          <a href="https://traversymedia.com"> Brad Traversy</a>
+          <a href="https://github.com/mass-gueye"> Mass Gueye</a>
         </strong>
         .
       </p>
